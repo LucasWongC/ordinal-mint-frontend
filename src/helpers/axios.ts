b88ce4eb98c0f98a09b5bb0a19b617ed7d7fefe9
LucasWongC <wm111898@gmail.com>
2024-05-06 07:27:49 -0700

    feat: backend config with ngrok

--- src/helpers/axios.ts
+++ src/helpers/axios.ts
@@ -5,6 +5,8 @@ const instance = axios.create({
   baseURL: BACKEND_URL,
   headers: {
     "Content-Type": "application/json",
+    "ngrok-skip-browser-warning": "69420",
+    Accept: "application/json",
   },
 });
 
