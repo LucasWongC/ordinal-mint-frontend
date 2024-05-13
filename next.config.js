cf317cf1bb43cd64d94a80423c35a44db3b21973
LucasWongC <wm111898@gmail.com>
2024-05-13 10:17:08 -0700

    fix: setup env for mainnet

--- next.config.js
+++ next.config.js
@@ -2,14 +2,14 @@
 const nextConfig = {
   // distDir: "build",
   rewrites: () => [
-    // {
-    //   source: "/api/:path*",
-    //   destination: "http://127.0.0.1:8001/api/:path*", // Proxy to Backend
-    // },
-    // {
-    //   source: "/api/docs",
-    //   destination: "http://127.0.0.1:8001/docs", // Proxy to Backend
-    // },
+    {
+      source: "/api/:path*",
+      destination: "http://127.0.0.1:8001/api/:path*", // Proxy to Backend
+    },
+    {
+      source: "/api/docs",
+      destination: "http://127.0.0.1:8001/docs", // Proxy to Backend
+    },
   ],
 };
 
