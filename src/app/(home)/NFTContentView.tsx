63f89e65a6ab97365e0f530871163b1ffc0f49db
LucasWongC <wm111898@gmail.com>
2024-06-14 03:26:14 -0700

    fix: css

--- src/app/(home)/NFTContentView.tsx
+++ src/app/(home)/NFTContentView.tsx
@@ -26,7 +26,7 @@ const NFTContentView: FC<Props> = ({ data }) => {
       >
         <p className="text-3xl font-bold text-white underline">{data.title}s</p>
         {isOpen && (
-          <div className="fixed left-1/2 top-1/2 z-20 w-[calc(100%-32px)] max-w-[556px] -translate-x-1/2 -translate-y-1/2 bg-gradient-to-b from-[#1A1A1AB2] to-[#071926B2] px-5 py-5">
+          <div className="max-h-90vh fixed left-1/2 top-1/2 z-20 w-[calc(100%-32px)] max-w-[556px] -translate-x-1/2 -translate-y-1/2 bg-gradient-to-b from-[#1A1A1AB2] to-[#071926B2] px-5 py-5">
             <div className="mb-10 text-center text-3xl font-semibold text-white md:text-4xl">
               <span className="underline">The {data.title}</span>:
             </div>
